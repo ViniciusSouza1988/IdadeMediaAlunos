9c6ee7c5467dd43bc825033d62f1bff153d76c75
ViniciusSouza1988 <viniciusdesouzasilveira1988@gmail.com>
2023-03-26 00:21:22 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿int Idade1 = 0;
+Console.WriteLine("Idade do Aluno 1 é: ");
+Idade1 = Convert.ToInt32(Console.ReadLine());
+
+int Idade2 = 0;
+Console.WriteLine("Idade do Aluno 2 é: ");
+Idade2 = Convert.ToInt32(Console.ReadLine());
+
+int Idade3 = 0;
+Console.WriteLine("Idade do Aluno 3 é: ");
+Idade3 = Convert.ToInt32(Console.ReadLine());
+
+int Idade4 = 0;
+Console.WriteLine("Idade do Aluno 4 é: ");
+Idade4 = Convert.ToInt32(Console.ReadLine());
+
+int Idade5 = 0;
+Console.WriteLine("Idade do Aluno 5 é: ");
+Idade5 = Convert.ToInt32(Console.ReadLine());
+
+int Idade6 = (Idade1 + Idade2 + Idade3 + Idade4 + Idade5) / 5;
+
+Console.WriteLine("A média de idade dos alunos é:" +  Idade6);
+
